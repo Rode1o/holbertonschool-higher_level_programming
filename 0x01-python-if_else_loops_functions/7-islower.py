de37883d083948eeb26a0b79a024f56bee129fc7
Rode1o <dumirola.dr@gmail.com>
2021-04-27 20:39:08 -0500

    fix PEP8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-	for charz in range(97, 122):
-		if charz == ord(c):
-			return 1
-	return 0
+    for charz in range(97, 122):
+        if charz == ord(c):
+            return 1
+    return 0
