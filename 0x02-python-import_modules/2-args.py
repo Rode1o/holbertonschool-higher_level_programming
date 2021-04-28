710b94387cd0aea042d685ac714786d79b79f26a
Rode1o <dumirola.dr@gmail.com>
2021-04-28 15:24:26 -0500

    test

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    n = len(sys.argv)
+    print("{} arguments:".format(n))
+    for i in range(1,n):
+        print(sys.argv[i], end ="")
