ff9ab92c94ee1d78b041748016e1ee7ca90bf2e9
Rode1o <dumirola.dr@gmail.com>
2021-04-28 19:03:03 -0500

    def argv2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,13 +9,13 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if argv[2] == '+':
+    if sys.argv[2] == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif argv[2] == '-':
+    elif sys.argv[2] == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif argv[2] == '*':
+    elif sys.argv[2] == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif argv[2] == '/':
+    elif sys.argv[2] == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
