71ac56250e8099c65b0755355c4727a9a9cda66b
Rode1o <dumirola.dr@gmail.com>
2021-05-05 21:22:35 -0700

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+def print_matrix_integer(matrix=[[]]):
+    for files in matrix:
+        token = 0
+        for column in files:
+            token += 1
+            print('{:d}'.format(column), end='')
+            if token != len(files):
+                print(" ", end='')
+        print('')
