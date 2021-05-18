e58a5cedf4e84a93d6716e62a1b526a02bc557c7
Rode1o <dumirola.dr@gmail.com>
2021-05-18 15:02:31 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 2-square.py)"""
+
+
+class Square:
+    """Write a class that defines a square by: (based on 2-square.py)"""
+    def __init__(self, size=0):
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
