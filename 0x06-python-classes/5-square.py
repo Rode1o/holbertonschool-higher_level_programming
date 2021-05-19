7b6f624a69bfa127a1aa8f6c302660ca143021b6
Rode1o <dumirola.dr@gmail.com>
2021-05-18 17:00:37 -0700

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""Write a class Square that defines a square by: (based on 4-square.py)"""
 
 
 class Square:
-    """"""
+    """Write a class that defines a square by: (based on 4-square.py)"""
     def __init__(self, size=0):
         self.__size = size
 
