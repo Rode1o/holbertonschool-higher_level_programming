0fda2d56333e90d0a66536864e11574392d4f63e
Rode1o <dumirola.dr@gmail.com>
2021-05-20 17:52:19 -0700

    task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""class Rectangle"""
 
 
 class Rectangle:
-    """"""
+    """class Rectangle"""
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
