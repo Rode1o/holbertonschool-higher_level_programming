e9a76f9950c70ef39080ee9eddea9d5af26162f0
Rode1o <dumirola.dr@gmail.com>
2021-05-31 15:41:48 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""writes a string to a text file (UTF8)"""
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and returns the number of characters written:"""
-    with open(filename,mode= "w", encoding = "utf-8") as my_file:
+    """writes a string to a text file (UTF8) and
+ returns the number of characters written:"""
+    with open(filename, mode="w", encoding="utf-8") as my_file:
         my_file.write(text)
     return len(text)
-        
