93243c0653ba0e986f71d21c437b6d15367fe1fd
Rode1o <dumirola.dr@gmail.com>
2021-05-31 15:35:51 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """holberton care berga"""
 
+
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, mode='r', encoding='utf-8') as my_file:
-        print(my_file.read())
+        print(my_file.read(), end="")
