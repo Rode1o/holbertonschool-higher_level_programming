af80950746b34fbb3286ba2467c4d802b8666c54
Rode1o <dumirola.dr@gmail.com>
2021-06-01 16:38:32 -0700

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""adds all arguments to a Python list"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import sys, json
+
+try:
+    textsaver = load_from_json_file('add_item.json')
+except:
+    textsaver = []
+
+for i in range(1, len(sys.argv)):
+    textsaver.append(sys.argv[i])
+save_to_json_file(textsaver, "add_item.json")
