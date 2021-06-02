fb852a25ec754347eb9fcb3d8e63cb7723eac8e5
Rode1o <dumirola.dr@gmail.com>
2021-06-01 17:37:57 -0700

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,41 +1,17 @@
 #!/usr/bin/python3
-""" Pascal Triangle """
+""" Defines pascal triangle """
 
 
 def pascal_triangle(n):
-    """
-    # Returns a list of lists of integers representing the Pascal’s triangle.
-
-    Args:
-
-        n (int) = Number of rows.
-    """
-
-    pascal_triangle = [[1]]
-    row = [1]
-
+    """ Prints pascal triangle """
     if n <= 0:
-        return ([])
-
-    for i in range(1, n):
-
-        if len(row) < 2:
-            row.append(1)
-            pascal_triangle.append(row)
-
-        else:
-
-            len_row = len(row)
-            row2 = []
-            row2.append(1)
-
-            for x in range(len(row)):
-
-                if (x + 1) < len(row):
-                    row2.append(row[x] + row[x + 1])
-
-            row2.append(1)
-            row = row2
-            pascal_triangle.append(row2)
-
-    return (pascal_triangle)
+        return []
+    tri = []
+    row = []
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            for j in range(i):
+                row.append(sum(tri[-1][j:j+2]))
+        tri.append(row)
+    return tri
