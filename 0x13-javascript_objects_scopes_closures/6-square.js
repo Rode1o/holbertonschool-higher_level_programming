032b24f08c6a1ff9bdddab00281184a4956f848b
Rode1o <dumirola.dr@gmail.com>
2021-08-03 02:08:12 -0500

    print X instead C

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Update square class
+const Square2 = require('./5-square.js');
+
+module.exports = class Square extends Square2 {
+    charPrint (c) {
+        if (!c) {
+            c = 'X';
+        }
+        for (let j = this.height; j; j--) {
+            console.log(c.repeat(this.width));
+        }
+    }
+};
