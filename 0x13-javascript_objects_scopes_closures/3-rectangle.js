89a38e138a31c0fa672f0e2f7cacc0e86198819c
Rode1o <dumirola.dr@gmail.com>
2021-08-03 01:36:14 -0500

    print squares like a instance method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/nodejs
+// Update class Rectangle
+
+module.exports = class Rectangle {
+    constructor (w, h) {
+        if (w > 0 && h > 0){
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print () {
+        for(; this.height; this.height--) {
+            console.log('X'.repeat(this.width));
+        }
+    }
+};
