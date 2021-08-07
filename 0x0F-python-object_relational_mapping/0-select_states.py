e44c90cc59e14fa557bd1a790d2917d8dd5f84a0
Rode1o <dumirola.dr@gmail.com>
2021-08-07 18:32:23 -0500

    filter state without ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-''' Lists all states from the databases '''
+"""Lists all states from the databases"""
 
 
 import MySQLdb
 from sys import argv
-if __name__ == '__main__':
-
-    connection = MySQLdb.connect(user=argv[1],
-                                 passwd=argv[2],
-                                 connection=argv[3],
-                                 host="localhost",
-                                 port=3306)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
-    states = cursor.fetchall()
-    for row in states:
-        print(state)
-    cursor.close()
-    connection.close()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
