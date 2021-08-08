a1b5244e44fdc4f7be4d0b5317fd8f3472f5acbf
Rode1o <dumirola.dr@gmail.com>
2021-08-07 21:52:58 -0500

    fix pep8 blank line

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,8 +7,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Empty class that inherits"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
