be74252424af541fed0a460840f4a7259aa680c1
Rode1o <dumirola.dr@gmail.com>
2021-08-19 21:43:11 -0500

    empty file

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers """
+
+
+def find_peak(list_of_integers):
+    """ Return a peak """
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    else:
+        return None
