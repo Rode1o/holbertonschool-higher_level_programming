5f7b980449991f8abd608616157d5025330c14d3
Rode1o <dumirola.dr@gmail.com>
2021-08-20 17:33:17 -0500

    cript that takes in a URL, sends a request to the URL and displays the value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status"""
+if __name__ == "__main__":
+    import urllib.request as request
+    from sys import argv
+    url = request.Request(argv[1])
+    with request.urlopen(url) as page:
+        print(page.headers.get('X-Request-Id'))
+        
