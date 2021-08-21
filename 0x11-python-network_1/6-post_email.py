2598a90bf33b25d81bc268fe346b1e264e302e47
Rode1o <dumirola.dr@gmail.com>
2021-08-20 21:23:21 -0500

    fix pep 8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status"""
+"""Python script that fetches
+https://intranet.hbtn.io/status"""
+
 if __name__ == "__main__":
     import requests
-    from sys import argv
+    import sys
     response = requests.post(sys.argv[1], {"email": sys.argv[2]}).text
     print(response)
