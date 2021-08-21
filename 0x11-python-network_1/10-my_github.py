cf27d9a7d4436872cda8882e89acd507b216188b
Rode1o <dumirola.dr@gmail.com>
2021-08-20 21:18:17 -0500

    import argv

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
 if __name__ == "__main__":
+    import requests
+    from sys import argv
+
     url = 'https://api.github.com/user'
     response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
     try:
         print(response.json().get("id"))
     except ValueError:
-        print("Not a valid JSON")~
+        print("Not a valid JSON")
