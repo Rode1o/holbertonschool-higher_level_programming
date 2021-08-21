3b4556e9721565c01dc9f9f8bd3b74ccbbfd1a70
Rode1o <dumirola.dr@gmail.com>
2021-08-20 21:19:27 -0500

    import argv

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status"""
-if __name__ == "__main__":
-    import requests
-    from sys import argv
+"""Python script that takes your GitHub credentials
+ (username and password) and uses the GitHub API to display your id"""
 
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    token = argv[2]
     url = 'https://api.github.com/user'
-    response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
-    try:
-        print(response.json().get("id"))
-    except ValueError:
-        print("Not a valid JSON")
+
+    r = requests.get(url, auth=(user, token))
+    print(r.json().get("id"))
